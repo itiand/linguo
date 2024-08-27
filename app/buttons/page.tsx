c67c34fda0abcbfdcef7937e65cd4b6807358b8a
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-08-27 14:29:11 -0400

    set up prettier tailwindcss

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const ButtonsPage = () => {
   return (
-    <div className="flex flex-col space-y-4 p-4 max-w-[200px]">
+    <div className="flex max-w-[200px] flex-col space-y-4 p-4">
       <Button size="sm">Primary</Button>
       <Button variant="secondary">Primary Outline</Button>
     </div>
