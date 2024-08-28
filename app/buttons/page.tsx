8bd1174352d3bf09629cedc77f914ab1a20cd980
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-08-28 15:41:06 -0400

    creating button variants

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -3,8 +3,13 @@ import { Button } from "@/components/ui/button";
 const ButtonsPage = () => {
   return (
     <div className="flex max-w-[200px] flex-col space-y-4 p-4">
-      <Button size="sm">Primary</Button>
-      <Button variant="secondary">Primary Outline</Button>
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
     </div>
   );
 };
