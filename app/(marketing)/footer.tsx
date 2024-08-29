cc404109e41d33db09894aa5091fbc26dcfbe3a6
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-08-29 18:58:50 -0400

    add flag svgs

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-slate-200 px-4">
+    <footer className="w-full border-t-2 border-slate-200 px-4 py-2">
       <h2>Footer</h2>
+      <Image src="/flags/ph.svg" alt="Philippines" width={50} height={50} />
     </footer>
   );
 };
