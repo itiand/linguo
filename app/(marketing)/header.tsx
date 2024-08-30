5fe86dfca0fa8f69ed5a11af79365c4c8e7a0374
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-08-30 14:02:55 -0400

    building header component - structure and clerk logic

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
-import { ClerkLoading } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
     <header className="w-full border-b-2 border-slate-200 px-4">
       <div>
-        <div id="linguo-logo">
+        {/* left side */}
+        <div id="linguo-logo" className="header-left">
           <Image
             src="/logo-placeholder-image.png"
             alt="Linguo"
@@ -13,7 +23,25 @@ const Header = () => {
           />
           <h1>Linguo</h1>
         </div>
-        <ClerkLoading></ClerkLoading>
+
+        {/* right side */}
+        <div className="header-right">
+          <ClerkLoading>
+            <Loader />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedIn>
+              <span>I am signed in</span>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton mode="modal" afterSignInUrl="/learn">
+                <Button>Login</Button>
+              </SignInButton>
+              <span>I am signed out</span>
+            </SignedOut>
+          </ClerkLoaded>
+        </div>
       </div>
     </header>
   );
