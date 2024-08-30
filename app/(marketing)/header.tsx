c883a4d32d0c878d1ffa8056a670378b55ccb612
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-08-30 16:38:12 -0400

    applying class styling on header and add linguo-green color

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -11,34 +11,36 @@ import { Loader } from "lucide-react";
 import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
-    <header className="w-full border-b-2 border-slate-200 px-4">
-      <div>
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="header-container mx-auto flex h-full items-center justify-between py-2 lg:max-w-screen-lg">
         {/* left side */}
-        <div id="linguo-logo" className="header-left">
+        <div id="linguo-logo" className="header-left flex items-center gap-x-2">
           <Image
             src="/logo-placeholder-image.png"
             alt="Linguo"
-            width={50}
-            height={50}
+            width={40}
+            height={40}
           />
-          <h1>Linguo</h1>
+          <h1 className="text-2xl font-bold tracking-wide text-[var(--linguo-green)]">
+            Linguo
+          </h1>
         </div>
 
         {/* right side */}
         <div className="header-right">
           <ClerkLoading>
-            <Loader />
+            <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
           </ClerkLoading>
           <ClerkLoaded>
             <SignedIn>
-              <span>I am signed in</span>
               <UserButton />
             </SignedIn>
             <SignedOut>
-              <SignInButton mode="modal" afterSignInUrl="/learn">
-                <Button>Login</Button>
+              <SignInButton mode="modal">
+                <Button size="lg" variant="ghost">
+                  Login
+                </Button>
               </SignInButton>
-              <span>I am signed out</span>
             </SignedOut>
           </ClerkLoaded>
         </div>
