3df5028607495513a8835cc3c3198f3a01f4c73b
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-09-30 21:43:44 -0400

    add flags

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Footer = () => {
   return (
     <footer className="hidden w-full border-t-2 border-slate-200 px-4 py-2 lg:block">
-      <div className="">
+      <div className="footer-container mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
         <Button size="lg" variant="ghost">
           <Image
             src="/flags/ph.svg"
@@ -14,7 +14,56 @@ const Footer = () => {
           />
           Tagalog
         </Button>
-        {/* 5 flags */}
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/flags/jp.svg"
+            alt="Japan"
+            width={20}
+            height={20}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/flags/cu.svg"
+            alt="Cuba"
+            width={20}
+            height={20}
+            className="mr-4 rounded-md"
+          />
+          Cuban Spanish
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/flags/mx.svg"
+            alt="Mexico"
+            width={20}
+            height={20}
+            className="mr-4 rounded-md"
+          />
+          Mexican Spanish
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/flags/tr.svg"
+            alt="Turkey"
+            width={20}
+            height={20}
+            className="mr-4 rounded-md"
+          />
+          Turkish
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/flags/tw.svg"
+            alt="Taiwan"
+            width={20}
+            height={20}
+            className="mr-4 rounded-md"
+          />
+          Mandarin Chinese
+        </Button>
       </div>
     </footer>
   );
